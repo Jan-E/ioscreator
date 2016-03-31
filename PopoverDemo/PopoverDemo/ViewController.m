00d0dda4eaf78f852a777fb1e07b7370c93eab25
Jan Ehrhardt <github@ehrhardt.nl>
2016-04-01 00:33:14 +0200

    Made the tableHeight min(cellCount, 10)

--- PopoverDemo/PopoverDemo/ViewController.m
+++ PopoverDemo/PopoverDemo/ViewController.m
@@ -39,10 +39,10 @@
     PopoverViewController *popVC =[[PopoverViewController alloc] initWithNibName:nil bundle:nil];
     // http://stackoverflow.com/a/26944036
     UINavigationController *popNav = [[UINavigationController alloc] initWithRootViewController:popVC];
-    popVC.preferredContentSize = CGSizeMake(280,450);
     popVC.cellNames = [[NSMutableArray alloc] init];
     [popVC.cellNames addObject:[NSString stringWithFormat:@"Project Alpha"]];
     [popVC.cellNames addObject:[NSString stringWithFormat:@"Project Beta"]];
+    popVC.preferredContentSize = CGSizeMake(280, (popVC.cellNames.count<10 ? 45*popVC.cellNames.count : 450));
     
     popNav.modalPresentationStyle = UIModalPresentationPopover;
     popNav.navigationBarHidden = YES;
@@ -50,7 +50,7 @@
     //_popOverP.delegate = self;
     _popOverP.sourceView = self.view;
     _popOverP.sourceRect = sender.frame;
-    _popOverP.permittedArrowDirections = UIPopoverArrowDirectionUp;
+    _popOverP.permittedArrowDirections = UIPopoverArrowDirectionDown;
     [self presentViewController:popNav animated:YES completion:nil];
 }
 
@@ -62,9 +62,35 @@
     popVC.cellNames = [[NSMutableArray alloc] init];
     [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 1"]];
     [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 2"]];
-    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Beta - 1"]];
-    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Beta - 2"]];
-    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Beta - 3"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 3"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 4"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 5"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 6"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 7"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 8"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 9"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 10"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 11"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 12"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 13"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 14"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 15"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 16"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 17"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 18"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 19"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 20"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 21"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 22"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 23"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 24"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 25"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 26"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 27"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 28"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 29"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Case Alpha - 30"]];
+    popVC.preferredContentSize = CGSizeMake(280, (popVC.cellNames.count<10 ? 45*popVC.cellNames.count : 450));
     popNav.modalPresentationStyle = UIModalPresentationPopover;
     popNav.navigationBarHidden = YES;
     _popOverP = popNav.popoverPresentationController;
@@ -79,10 +105,21 @@
     PopoverViewController *popVC =[[PopoverViewController alloc] initWithNibName:nil bundle:nil];
     // http://stackoverflow.com/a/26944036
     UINavigationController *popNav = [[UINavigationController alloc] initWithRootViewController:popVC];
-    popVC.preferredContentSize = CGSizeMake(280,450);
     popVC.cellNames = [[NSMutableArray alloc] init];
     [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 1"]];
     [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 2"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 3"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 4"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 5"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 6"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 7"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 8"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 9"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 10"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 11"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Session 12"]];
+    [popVC.cellNames addObject:[NSString stringWithFormat:@"Please rename me"]];
+    popVC.preferredContentSize = CGSizeMake(280, (popVC.cellNames.count<10 ? 45*popVC.cellNames.count : 450));
     popNav.modalPresentationStyle = UIModalPresentationPopover;
     popNav.navigationBarHidden = YES;
     _popOverP = popNav.popoverPresentationController;

be50bf0e76152b44de9de03a4d6aa002a547ffee
TFR <TFR@tfrs-mbp.fritz.box>
2016-04-03 20:18:25 +0200

    Preselections for demo copy/paste fix

--- PopoverDemo/PopoverDemo/ViewController.m
+++ PopoverDemo/PopoverDemo/ViewController.m
@@ -39,7 +39,7 @@
     self.projectIndex = 1;
     self.caseIndex = 14;
     self.sessionIndex = 10;
-    NSLog(@"Preselections for demo: %d %d %d", self.projectIndex, self.projectIndex, self.projectIndex);
+    NSLog(@"Preselections for demo: %d %d %d", self.projectIndex, self.caseIndex, self.sessionIndex);
 }
 
 - (void)didReceiveMemoryWarning
